ea934cf74f2a5b99dc76479eda755af5542ab74d
Nat! <nat@mulle-kybernetik.com>
2018-03-26 02:12:01 +0200

    * fix dox a bit, but more work to do

--- mulle-project/version-info.sh
+++ mulle-project/version-info.sh
@@ -6,6 +6,6 @@
 #
 VERSIONFILE="CMakeLists.txt"
 VERSIONNAME="PROJECT_VERSION"
-VERSIONFILEMIRRORS="src/mulle-c/c/etc/version/mulle-c/c
-src/mulle-c/cmake/etc/version/mulle-c/cmake
-src/mulle-c/cmake-c/etc/version/mulle-c/cmake-c"
+VERSIONFILEMIRRORS="src/mulle-c/c/share/version/mulle-c/c
+src/mulle-c/cmake/share/version/mulle-c/cmake
+src/mulle-c/cmake-c/share/version/mulle-c/cmake-c"
