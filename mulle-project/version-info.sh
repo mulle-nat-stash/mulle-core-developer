1f9c05c226646c9306fa0cbffdd9019e7b14ab0a
Nat! <nat-tv@mulle-kybernetik.com>
2018-05-25 00:33:38 +0200

    * fix homebrew hopefully

--- mulle-project/version-info.sh
+++ mulle-project/version-info.sh
@@ -6,6 +6,6 @@
 #
 VERSIONFILE="CMakeLists.txt"
 VERSIONNAME="PROJECT_VERSION"
-VERSIONFILEMIRRORS="src/mulle-c/c/share/version/mulle-c/c
-src/mulle-c/cmake/share/version/mulle-c/cmake
-src/mulle-c/c-cmake/share/version/mulle-c/c-cmake"
+VERSIONFILEMIRRORS="src/mulle-c/c/version
+src/mulle-c/cmake/version
+src/mulle-c/c-cmake/version"
